36e1fbed431378aa49ce33340dd792afff671e9e
Warren Pitallo <wpitallo@gmail.com>
2024-07-14 12:47:14 +0200

    update

--- src/apps/championFan/pages/Page1.jsx
+++ src/apps/championFan/pages/Page1.jsx
@@ -1,11 +1,58 @@
+import PropTypes from 'prop-types';
 import PageTemplate from './PageTemplate';
-import PropTypes from 'prop-types'
+import styles from './Page1.module.scss';
 
 function Page({ isVisible }) {
   return (
     <PageTemplate isVisible={isVisible}>
       <h1>Page 1</h1>
-      {/* Your page content */}
+      <div className={styles.container}>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+        <div className={styles.square}>
+          <div className={styles['square-content']}></div>
+        </div>
+      </div>
     </PageTemplate>
   );
 }
--- src/apps/championFan/pages/PageTemplate.jsx
+++ src/apps/championFan/pages/PageTemplate.jsx
@@ -4,10 +4,12 @@ import styles from './PageTemplate.module.scss';
 function PageTemplate({ isVisible, children }) {
     return (
         <div className={styles.page}>
-            <div className={styles['background-image']}></div>
-            <div className={`${styles['content-container']} ${isVisible ? styles.visible : ''}`}>
-                <div className={styles['content-box']}>
-                    {children}
+            <div className={styles['scroll-container']}>
+                <div className={styles['background-image']}></div>
+                <div className={`${styles['content-container']} ${isVisible ? styles.visible : ''}`}>
+                    <div className={styles['content-box']}>
+                        {children}
+                    </div>
                 </div>
             </div>
         </div>
