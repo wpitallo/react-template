ae22f2e814a74d4c588605f243cf4446fc633fca
Warren Pitallo <wpitallo@gmail.com>
2024-07-24 23:35:43 +0200

    Avataaars

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from '@app/App'
+//import App from '@app/App'
+
+import Avatar from '@components/avatar/Avatar'
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
 
-root.render(<App />)
+// root.render(<App />)
+root.render(<Avatar />)
